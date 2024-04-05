17363d9ac11e8367555329c10c5d11debfdbd1fb
Jhollen93 <140207469+Jhollen93@users.noreply.github.com>
2024-04-05 15:32:00 -0500

    Add files via upload

---
+++ LeNet5.cpp
@@ -0,0 +1,33 @@
+using namespace tiny_dnn;
+using namespace tiny_dnn::layers;
+
+network<sequential> net;
+
+net << conv(32, 32, 5, 1, 6) << tanh_layer() 
+    << ave_pool(28, 28, 6, 2) << tanh_layer()
+    << conv(14, 14, 5, 6, 16) << tanh_layer()
+    << ave_pool(10, 10, 16, 2) << tanh_layer()
+    << conv(5, 5, 5, 16, 120) << tanh_layer()
+    << fc(120, 84) << tanh_layer()
+    << fc(84, 10);
+
+std::string data_dir_path = "path_to_your_data_directory";
+std::vector<label_t> train_labels, test_labels;
+std::vector<vec_t> train_images, test_images;
+
+parse_mnist_labels(data_dir_path + "/train-labels.idx1-ubyte", &train_labels);
+parse_mnist_images(data_dir_path + "/train-images.idx3-ubyte", &train_images, -1.0, 1.0, 2, 2);
+parse_mnist_labels(data_dir_path + "/t10k-labels.idx1-ubyte", &test_labels);
+parse_mnist_images(data_dir_path + "/t10k-images.idx3-ubyte", &test_images, -1.0, 1.0, 2, 2);
+
+adagrad optimizer;
+net.train<cross_entropy>(optimizer, train_images, train_labels, batch_size, num_epochs);
+
+size_t batch_size = 10; // Example batch size
+int num_epochs = 30; // Example number of epochs
+
+net.train<cross_entropy>(optimizer, train_images, train_labels, batch_size, num_epochs);
+
+net.test(test_images, test_labels).print_detail(std::cout);
+
+net.save("lenet-model");
